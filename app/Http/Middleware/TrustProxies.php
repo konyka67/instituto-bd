e6d2a096f26dee156fc2819f7959764d259f9147
juanchaco67 <nuevojuanchaco67@gmail.com>
2020-03-18 21:24:40 -0500

    cambiar proxy

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -7,26 +7,6 @@ use Illuminate\Http\Request;
 
 class TrustProxies extends Middleware
 {
-   /**
-     * The trusted proxies for this application.
-     *
-     * @var array
-     */
-    protected $proxies = '**';
-
-    /**
-     * The current proxy header mappings.
-     *
-     * @var array
-     */
-    
-    protected $headers = [
-
-            Illuminate\Http\Request::HEADER_FORWARDED    => null,
-            Illuminate\Http\Request::HEADER_CLIENT_IP    => 'X_FORWARDED_FOR',
-            Illuminate\Http\Request::HEADER_CLIENT_HOST  => null,
-            Illuminate\Http\Request::HEADER_CLIENT_PROTO => 'X_FORWARDED_PROTO',
-            Illuminate\Http\Request::HEADER_CLIENT_PORT  => 'X_FORWARDED_PORT',
-
-    ];
+    protected $proxies = '*';
+    protected $headers = Request:: HEADER_X_FORWARDED_AWS_ELB;
 }
